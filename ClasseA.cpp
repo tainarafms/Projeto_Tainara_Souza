70948477e44b2ba49fd5039eaeed4d9e59cd86ad
Tainara Souza <tainarafms7@gmail.com>
2026-04-18 22:30:59 -0300

    Criado o Método MA3()

--- ClasseA.cpp
+++ ClasseA.cpp
@@ -2,24 +2,20 @@
 using namespace std;
 
 class ClasseA {
-private:
-    int A1;   // Atributo int A1 
-    float A2; // Atributo float A2 
+    private:
+        int A1;
+        float A2;
 
-public:
-    // Métodos Get e Set 
-    int getA1() { return A1; }
-    void setA1(int val) { A1 = val; }
+    public:
+        void MA1() {
+            cout << "MA1" << endl;
+        }
 
-    float getA2() { return A2; }
-    void setA2(float val) { A2 = val; }
+        void MA2() {
+            cout << "MA2" << endl;
+        }
 
-    // Métodos que imprimem o próprio nome
-    void MA1() {
-        cout << "MA1" << endl;
-    }
-
-    void MA2() {
-        cout << "MA2" << endl;
-    }
+        void MA3() {
+            cout << "Alteração a classe A partir do clone" << endl;
+        }
 };
