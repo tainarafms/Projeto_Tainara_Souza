8a2569c72ba3880a4a9a1c14437266519857e143
Tainara Souza <tainarafms7@gmail.com>
2026-04-18 22:28:17 -0300

    Criação da Classe C

---
+++ ClasseC.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+class ClasseC {
+    private:
+        string C1;
+        int C2;
+
+    public:
+        void MC1() {
+            cout << "MC1" << endl;
+        }
+
+        void MC2() {
+            cout << "MC2" << endl;
+        }
+};
